c836bc2475b1f74f79add78e775d45083da52449
sdelp66 <sdelp66@gmail.com>
2022-10-27 19:32:42 -0300

    subido homework 07-JS-VI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -3,44 +3,75 @@
 function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
-  //Tu código:
+  //Tu código: 1
+   
+  return nombre.charAt(0).toUpperCase()+nombre.substr(1,nombre.length);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
-  //Tu código:
+  //Tu código: 2
+
+  return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
-  //Tu código:
+  //Tu código: 3
+
+  return cb(n1,n2);
+
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
-  //Tu código:
+  //Tu código: 4
+
+  let suma=0;
+  for (let i=0;i<numeros.length;i++){
+    suma=suma+numeros[i];
+  }
+  cb(suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
-  //Tu código:
+  //Tu código: 5
+
+  array.forEach(function(elemento){
+    cb(elemento);
+  });
 }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
-  //Tu código:
+  //Tu código: 6
+
+  const nvoArray=[];
+  array.forEach(function(elemento){
+    nvoArray.push(cb(elemento));
+  });
+  return nvoArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
-  //Tu código:
+  //Tu código: 7
+
+  const nvoArray=[];
+  array.forEach(function(elemento){
+    if(elemento.charAt(0)=='a'){
+      nvoArray.push(elemento);
+    }
+  });
+  return nvoArray;
 }
 
 // No modificar nada debajo de esta línea
